cc5340246770ebfa6ffdc43c45c0e3ae51f13063
dnaHunter <daniel.aston@live.co.uk>
2025-10-13 12:56:42 +0100

    Set up route for header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/polls/:pollId" element={<></>} />
+        </Routes>
+        <Header />
+        <Routes>
+          <Route path="/" element={<></>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,8 @@
+import "./Header.scss"
+
+export default function Header(){
+return <section className="header">
+    <p className="header__logo">Questionable</p>
+    <p className="header__tab">Create nestable poll</p>
+</section>
+}
